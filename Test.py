39fe76c5fcb1a61d017e2a9b88a5ef764394033b
atboxshop <atboxhsop@gmail.com>
2023-01-02 02:08:37 +0000

    commit

---
+++ Test.py
@@ -0,0 +1,15 @@
+# publish.py
+import pika, os
+
+# Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://user1:user@192.168.31.30:5672/%2f')
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='hello') # Declare a queue
+channel.basic_publish(exchange='',
+                      routing_key='Hello World',
+                      body='Hello CloudAMQP!')
+
+print(" [x] Sent 'Hello World!'")
+connection.close()
