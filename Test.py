1ad1e3d868b3ee2b637feca2d6972d9d7db302fd
atboxshop <atboxhsop@gmail.com>
2023-01-02 03:59:57 +0000

    change

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@
 import pika, os
 
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
-url = os.environ.get('CLOUDAMQP_URL', 'amqp://user1:user@192.168.31.30:5672/%2f')
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://user1:user@192.168.31.30:15672/%2f')
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
 channel = connection.channel() # start a channel
